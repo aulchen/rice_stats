e1f5ba8263dc46e0e31eb33d6277bbfd9d331279
Arthur Chen <28554289+aulchen@users.noreply.github.com>
2021-07-27 15:48:24 -0700

    Worked on Problem 11

--- Chapter9/Problem11.R
+++ Chapter9/Problem11.R
@@ -1,5 +1,22 @@
-distSeq <- c(seq(-10, -0.1, by = .1), seq(0.1, 10, by=.1))
-n = 25
-y = qchisq(.95, df = 1)
-testPower <-function(mu) pnorm(-sqrt(y) + ((sqrt(n)*mu)/10)) + pnorm(-sqrt(y) - ((sqrt(n)*mu)/10))
-plot(distSeq, testPower(distSeq), type = 'l')
+distSeq <- seq(-10, 10, by = .1)
+testPower <-function(mu, n, c) pnorm(-sqrt(c) - ((sqrt(n)*mu)/10)) + pnorm(-sqrt(c) + ((sqrt(n)*mu)/10))
+
+png('../Desktop/Projects/Stat135/Chapter9/output/Problem11/n=25.png', type = 'cairo',
+    width = 640, height = 480)
+plot(distSeq, testPower(distSeq, n = 25, c = qchisq(.95, df = 1)), type = 'l',
+     xlab = 'Mu', ylab = 'Power', main = 'Power of the Likelihood Test, n=25')
+points(0, testPower(0, n = 25, c = qchisq(.95, df = 1)), pch = 21)
+lines(distSeq, testPower(distSeq, n = 25, c = qchisq(.90, df = 1)), type = 'l', lty = 3)
+points(0, testPower(0, n = 25, c = qchisq(.90, df = 1)), pch = 21)
+legend(title = 'Alpha', x = 'bottomright', legend = c('.10', '.05'), lty = c(3, 1))
+dev.off()
+
+png('../Desktop/Projects/Stat135/Chapter9/output/Problem11/n=100.png', type = 'cairo',
+    width = 640, height = 480)
+plot(distSeq, testPower(distSeq, n = 100, c = qchisq(.95, df = 1)), type = 'l',
+     xlab = 'Mu', ylab = 'Power', main = 'Power of the Likelihood Test, n=100')
+points(0, testPower(0, n = 25, c = qchisq(.95, df = 1)), pch = 21)
+lines(distSeq, testPower(distSeq, n = 100, c = qchisq(.90, df = 1)), type = 'l', lty = 3)
+points(0, testPower(0, n = 25, c = qchisq(.90, df = 1)), pch = 21)
+legend(title = 'Alpha', x = 'bottomright', legend = c('.10', '.05'), lty = c(3, 1))
+dev.off()
